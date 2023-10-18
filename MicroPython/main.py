e9d8e5ccdecc8263b19355be018b4623e7b475f1
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-10-18 12:24:01 -0400

    fixed py code

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -27,17 +27,20 @@ neopixel_strip.show()
 
 while True:
     if button_a.is_pressed():
-
+        display.clear()
+        # if amount_of_light is <= 51
         if amount_of_light <= 51:
             neopixel_strip[0] = (0, 0, 0)
             print(neopixel_strip[0])
             neopixel_strip.show()
 
+        # if amount_of_light is >= 52
         if amount_of_light >= 52:
             neopixel_strip[0] = (0, 0, 255)
             print(neopixel_strip[0])
             neopixel_strip.show()
 
+        # if amount_of_light is > 104
         if amount_of_light > 104:
             neopixel_strip[0] = (0, 0, 255)
             neopixel_strip[1] = (0, 0, 255)
@@ -45,6 +48,7 @@ while True:
             print(neopixel_strip[1])
             neopixel_strip.show()
 
+        # if amount_of_light is > 156
         if amount_of_light > 156:
             neopixel_strip[0] = (0, 0, 255)
             neopixel_strip[1] = (0, 0, 255)
@@ -54,6 +58,7 @@ while True:
             print(neopixel_strip[2])
             neopixel_strip.show()
 
+        # if amount_of_light is > 208
         if amount_of_light > 208:
             neopixel_strip[0] = (0, 0, 255)
             neopixel_strip[1] = (0, 0, 255)
@@ -64,3 +69,16 @@ while True:
             print(neopixel_strip[2])
             print(neopixel_strip[3])
             neopixel_strip.show()
+
+    # reset light level
+    if button_b.is_pressed():
+        display.clear()
+        neopixel_strip[0] = (0, 0, 0)
+        neopixel_strip[1] = (0, 0, 0)
+        neopixel_strip[2] = (0, 0, 0)
+        neopixel_strip[3] = (0, 0, 0)
+        print(neopixel_strip[0])
+        print(neopixel_strip[1])
+        print(neopixel_strip[2])
+        print(neopixel_strip[3])
+        neopixel_strip.show()
